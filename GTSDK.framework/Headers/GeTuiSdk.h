4c06fdd11d11e4ace65448009ea537ce0c918312
linwanting <linwt@getui.com>
2020-03-31 15:12:14 +0800

    update：更新2.4.3.0-noidfa版本

--- GTSDK.framework/Headers/GeTuiSdk.h
+++ GTSDK.framework/Headers/GeTuiSdk.h
@@ -5,7 +5,7 @@
 //  Created by gexin on 15-5-5.
 //  Copyright (c) 2015年 Gexin Interactive (Beijing) Network Technology Co.,LTD. All rights reserved.
 //
-//  GTSDK-Version: 2.4.3.0
+//  GTSDK-Version: 2.4.3.0-noidfa
 
 #import <Foundation/Foundation.h>
 
