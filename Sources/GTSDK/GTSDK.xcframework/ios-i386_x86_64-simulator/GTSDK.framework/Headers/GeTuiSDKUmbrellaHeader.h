b27a83678fe446a4eb9857581de8ba249fab4907
linwanting <linwt@getui.com>
2021-09-03 15:10:03 +0800

    update: sdk

---
+++ Sources/GTSDK/GTSDK.xcframework/ios-i386_x86_64-simulator/GTSDK.framework/Headers/GeTuiSDKUmbrellaHeader.h
@@ -0,0 +1,15 @@
+//
+//  GeTuiSDKUmbrellaHeader.h
+//  GeTuiSDKUmbrella_IOS
+
+#ifndef GeTuiSDKUmbrellaHeader_h
+#define GeTuiSDKUmbrellaHeader_h
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double StaticLibraryModuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char StaticLibraryModuleVersionString[];
+
+#import "GeTuiSdk.h
+
+#endif /* GeTuiSDKUmbrellaHeader_h */
