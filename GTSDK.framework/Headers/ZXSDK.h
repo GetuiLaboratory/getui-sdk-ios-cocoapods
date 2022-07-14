072da616afd35ef44d94216288286c6d97208a29
linwanting <linwt@getui.com>
2022-07-14 15:05:57 +0800

    update: 更新2.6.7.0-noidfa版本

--- GTSDK.framework/Headers/ZXSDK.h
+++ GTSDK.framework/Headers/ZXSDK.h
@@ -3,7 +3,7 @@
 //  ZXSDK
 //
 //  Created by zx on 2021-03-10.
-//  ZXSDK-Version: 2.0.0.7785
+//  ZXSDK-Version: 3.0.0.10551
 
 #import <Foundation/Foundation.h>
 
@@ -12,13 +12,22 @@ FOUNDATION_EXPORT double ZXSDKVersionNumber;
 
 //! Project version string for ZXSDK.
 FOUNDATION_EXPORT const unsigned char ZXSDKVersionString[];
+
+#define ZXSDKStoreDATA(sectname) __attribute((used, section("__DATA,"#sectname" ")))
+
+#define ZXSDKStoreMod(name, value) \
+class ZXSDK; char * k##name##_mod ZXSDKStoreDATA(ZXSDKMods) = ""#value"";
+
 @class ZXSDKResultModel;
 typedef void (^ZXSDKCallback)(ZXSDKResultModel *_Nullable model, NSError *_Nullable error);
+typedef void (^ZXSDKUAIDCallback)(NSString *_Nullable uaid, NSError *_Nullable error);
+typedef void (^ZXSDKAidInfoCallback)(NSDictionary *_Nullable res, NSError *_Nullable error);
 typedef void (^ZXSDKVerifyCallback)(BOOL success, NSError *_Nullable error);
 
 @interface ZXSDKResultModel : NSObject
 @property (nonatomic, copy, nonnull) NSString *zxid;
 @property (nonatomic, copy, nonnull) NSString *expireTime;
+@property (nonatomic, copy, nullable) NSArray *tags;
 
 @property (nonatomic, copy, nullable) NSString *aaid;
 @property (nonatomic, copy, nullable) NSString *vaid;
@@ -27,10 +36,23 @@ typedef void (^ZXSDKVerifyCallback)(BOOL success, NSError *_Nullable error);
 @protocol ZXSDKProtocol <NSObject>
 
 /// 获取UAID
-- (void)getUAID:(nonnull void (^)(NSString *_Nullable uaid, NSError *_Nullable error))callback;
+/// @param accessKeyId 通过管理后台申请分配的密钥标识
+/// @param signature 信息签名字符串，参考签名方式。
+/// @param signatureMethod 信息签名方法，目前固定为 HMAC-SHA256 。
+/// @param signatureNonce 64 字节以内的随机串，用于防止重放攻击，每次请求 必须提供不同的值。
+/// @param timestamp UTC时间戳，从UTC1970年1月1日0时0分0秒起至现在 的总秒数。如 1632634877
+- (void)getUAIDWithAccessKeyId:(NSString *_Nonnull)accessKeyId
+                     signature:(NSString *_Nonnull)signature
+               signatureMethod:(NSString *_Nonnull)signatureMethod
+                signatureNonce:(NSString *_Nonnull)signatureNonce
+                     timestamp:(NSString *_Nonnull)timestamp
+                      callback:(ZXSDKUAIDCallback _Nonnull)callback;
 
 /// 获取卓信id
 - (void)getZXID:(ZXSDKCallback _Nonnull)callback;
+
+/// 获取设备对应Tag
+- (void)getTag:(ZXSDKAidInfoCallback _Nonnull)callback;
 
 /// 读取卓信ID开关
 - (BOOL)enable;
@@ -53,13 +75,26 @@ typedef void (^ZXSDKVerifyCallback)(BOOL success, NSError *_Nullable error);
 
 @interface ZXSDK : NSObject
 
-+ (void)startSdkWithAppId:(NSString * _Nullable)appId callback:(ZXSDKCallback _Nonnull)callback;
++ (void)startZXSDK;
 
 /// 获取卓信id
 + (void)getZXID:(ZXSDKCallback _Nonnull)callback;
 
 /// 获取UAID
-+ (void)getUAID:(nonnull void (^)(NSString *_Nullable uaid, NSError *_Nullable error))callback;
+/// @param accessKeyId 通过管理后台申请分配的密钥标识
+/// @param signature 信息签名字符串，参考签名方式。
+/// @param signatureMethod 信息签名方法，目前固定为 HMAC-SHA256 。
+/// @param signatureNonce 64 字节以内的随机串，用于防止重放攻击，每次请求 必须提供不同的值。
+/// @param timestamp UTC时间戳，从UTC1970年1月1日0时0分0秒起至现在 的总秒数。如 1632634877
++ (void)getUAIDWithAccessKeyId:(NSString *_Nonnull)accessKeyId
+                     signature:(NSString *_Nonnull)signature
+               signatureMethod:(NSString *_Nonnull)signatureMethod
+                signatureNonce:(NSString *_Nonnull)signatureNonce
+                     timestamp:(NSString *_Nonnull)timestamp
+                      callback:(ZXSDKUAIDCallback _Nonnull)callback;
+
+/// 获取设备对应Tag
++ (void)getTag:(ZXSDKAidInfoCallback _Nonnull)callback;
 
 /// 读取卓信ID开关
 + (BOOL)enable;
@@ -78,6 +113,6 @@ typedef void (^ZXSDKVerifyCallback)(BOOL success, NSError *_Nullable error);
 /// sdk版本号
 + (NSString * _Nonnull)version;
 
-/// 服务商使用, 普通用户不建议使用
+/// 服务商使用, 普通开发不建议使用
 + (id<ZXSDKProtocol>_Nullable)initWithAppId:(NSString *_Nonnull)appId;
 @end
