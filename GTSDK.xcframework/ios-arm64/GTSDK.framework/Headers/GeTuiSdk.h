de70cbaede9528e7f418d29af1b6305321c97324
linwanting <linwt@getui.com>
2026-07-30 11:06:11 +0800

    update: 更新3.1.1.0版本

--- GTSDK.xcframework/ios-arm64/GTSDK.framework/Headers/GeTuiSdk.h
+++ GTSDK.xcframework/ios-arm64/GTSDK.framework/Headers/GeTuiSdk.h
@@ -5,7 +5,7 @@
 //  Created by gexin on 15-5-5.
 //  Copyright (c) 2015年 Gexin Interactive (Beijing) Network Technology Co.,LTD. All rights reserved.
 //
-//  GTSDK-Version: 3.1.0.0
+//  GTSDK-Version: 3.1.1.0
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
